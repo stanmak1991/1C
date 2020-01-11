a615706292fe98c6479dc76047d7141b08d6c8e3
Stanislav Makovetskiy <stanislav_makovetskiy@smartfoxpro.com>
2020-01-11 14:57:24 +0200

    add changes

--- src/main/java/ru/openbank/marketplace/front/controller/OneCController.java
+++ src/main/java/ru/openbank/marketplace/front/controller/OneCController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.openbank.marketplace.front.types.BankPartyType;
+import ru.openbank.marketplace.front.types.CustomerPartyType;
+import ru.openbank.marketplace.front.types.GetPacketListResponseType;
 import ru.openbank.marketplace.front.types.GetSettingsResponseType;
 import ru.openbank.marketplace.front.types.LogonResponseType;
 import ru.openbank.marketplace.front.types.Packet;
+import ru.openbank.marketplace.front.types.ParticipantType;
+import ru.openbank.marketplace.front.types.Probe;
 import ru.openbank.marketplace.front.types.ResultBank;
+import ru.openbank.marketplace.front.types.SendPacketResponseType;
 import ru.openbank.marketplace.front.types.SuccessResultType;
 
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -18,7 +25,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 @RestController
-@RequestMapping(value = "/", produces = MediaType.APPLICATION_XML_VALUE)
+@RequestMapping(value = "/1с", produces = MediaType.APPLICATION_XML_VALUE)
 public class OneCController {
 
     @RequestMapping(value = "/Logon", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
@@ -31,11 +38,12 @@ public class OneCController {
         successResultType.setLogonResponse(logonResponse);
         ResultBank resultBank = new ResultBank();
         resultBank.setSuccess(successResultType);
+        resultBank.setFormatVersion(apiVersion);
         return resultBank;
     }
 
     @RequestMapping(value = "/GetSettings", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-    public ResultBank GetSettings(@RequestHeader(name = "CustomerID") String customerId,
+    public ResultBank getSettings(@RequestHeader(name = "CustomerID") String customerId,
                                   @RequestHeader(name = "Account") String account,
                                   @RequestHeader(name = "Inn") String inn,
                                   @RequestHeader(name = "Bic") String bic,
@@ -48,20 +56,76 @@ public class OneCController {
         getSettingsResponse.getData().setValue(BytesTemp.bytes);
         XMLGregorianCalendar calendar = new XMLGregorianCalendarImpl(new GregorianCalendar(2015, Calendar.MARCH, 19, 11, 21, 2));
         getSettingsResponse.setCreationDate(calendar);
-        getSettingsResponse.setFormatVersion("2.2.1");
-        getSettingsResponse.setId("502036");
+        getSettingsResponse.setId(customerId);
         SuccessResultType type = new SuccessResultType();
         type.setGetSettingsResponse(getSettingsResponse);
         ResultBank bank = new ResultBank();
         bank.setSuccess(type);
+        bank.setFormatVersion(apiVersion);
         return bank;
     }
 
     @RequestMapping(value = "/SendPack", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-    public ResultBank SendPack(@RequestHeader(name = "CustomerID", required = true) String customerId,
+    public ResultBank sendPack(@RequestHeader(name = "CustomerID", required = true) String customerId,
                                @RequestHeader(name = "SID", required = true) String sid,
                                @RequestHeader(name = "ApiVersion", required = true) String apiVersion,
                                @RequestBody Packet packet) {
-        return new ResultBank();
+        ResultBank bank = new ResultBank();
+        bank.setFormatVersion(apiVersion);
+        SendPacketResponseType responseType = new SendPacketResponseType();
+        responseType.setID("50214584626");
+        SuccessResultType successResultType = new SuccessResultType();
+        successResultType.setSendPacketResponse(responseType);
+        bank.setSuccess(successResultType);
+        return bank;
+    }
+
+    @RequestMapping(value = "/GetPackList", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+    public ResultBank getPackList(@RequestHeader(name = "CustomerID", required = true) String customerId,
+                                  @RequestHeader(name = "SID", required = true) String sid,
+                                  @RequestHeader(name = "ApiVersion", required = true) String apiVersion,
+                                  @RequestParam(name = "date") String dateTime) {
+        GetPacketListResponseType getPacketListResponse = new GetPacketListResponseType();
+        getPacketListResponse.setTimeStampLastPacket(
+                new XMLGregorianCalendarImpl(new GregorianCalendar(2015, Calendar.FEBRUARY, 19, 11, 15, 42)));
+        getPacketListResponse.getPacketID().add("50214585876");
+        SuccessResultType successResultType = new SuccessResultType();
+        successResultType.setGetPacketListResponse(getPacketListResponse);
+        ResultBank bank = new ResultBank();
+        bank.setSuccess(successResultType);
+        return bank;
+    }
+
+    @RequestMapping(value = "/GetPack", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+    public ResultBank getPack(@RequestHeader(name = "CustomerID", required = true) String customerId,
+                              @RequestHeader(name = "SID", required = true) String sid,
+                              @RequestHeader(name = "ApiVersion", required = true) String apiVersion,
+                              @RequestParam(name = "id") String id) {
+        Packet packet = new Packet();
+        packet.setUserAgent("Back Office");
+        packet.setId(id);
+        XMLGregorianCalendar calendar = new XMLGregorianCalendarImpl(new GregorianCalendar(2015, Calendar.FEBRUARY, 19, 11, 15, 42));
+        packet.setCreationDate(calendar);
+        packet.setFormatVersion(apiVersion);
+        BankPartyType bankPartyType = new BankPartyType();
+        bankPartyType.setBic("044525888");
+        ParticipantType sender = new ParticipantType();
+        sender.setBank(bankPartyType);
+        packet.setSender(sender);
+        ParticipantType recipient = new ParticipantType();
+        CustomerPartyType customerPartyType = new CustomerPartyType();
+        customerPartyType.setId(customerId);
+        recipient.setCustomer(customerPartyType);
+        packet.setRecipient(recipient);
+        SuccessResultType successResultType = new SuccessResultType();
+        successResultType.setGetPacketResponse(packet);
+        ResultBank bank = new ResultBank();
+        bank.setSuccess(successResultType);
+        return bank;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
+    public void getPack(@RequestBody Probe probe) {
+        return;
     }
 }
